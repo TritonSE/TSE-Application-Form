d1c8e2e3b4c97eea945a4562ae694c4e0b9b429c
EShults2124 <shults.eric01@gmail.com>
2022-05-20 15:35:08 -0700

    Added Centered checkboxes with disabling functionality

--- frontend/src/components/ApplicationForm.jsx
+++ frontend/src/components/ApplicationForm.jsx
@@ -1,9 +1,52 @@
 import Form from "react-bootstrap/Form"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ApplicationForm = (props) => {
   // initialize state below this line
 
+  const [roles, setRoles] = useState(
+    {
+    TestDev: false,
+    TestDes: false,
+    Dev: false,
+    Des: false,
+    ProdMan: false
+    }
+  );
+  
+
   // create any event handler functions below this line
+
+  let handleCheckbox = (label) => {
+
+    // have to relogic this because it needs to not undo if just one is unchecked
+
+    console.log(label);
+    switch(label) {
+      case "TEST Developer":
+        setRoles({...roles, TestDev: !roles.TestDev});
+        break;
+      case "TEST Designer":
+        setRoles({...roles, TestDes: !roles.TestDes});
+        break;
+      case "Designer":
+        setRoles({...roles, Des: !roles.Des});
+        break;
+      case "Developer":
+        setRoles({...roles, Dev: !roles.Dev});
+        break;
+      case "Product Manager":
+        setRoles({...roles, ProdMan: !roles.ProdMan});
+        break;
+      default:
+        console.log("ERROR");
+    }
+}
 
   // all html related material below here
   return (
@@ -12,6 +55,66 @@ const ApplicationForm = (props) => {
       {/* Personal Information Section below this line*/}
 
       {/* Checkboxes Section below this line*/}
+
+      <Form.Group>
+      
+        <Container>
+        <Row className="justify-content-md-center">
+          <Form.Label class="text-center">Which roles at TSE are you interested at applying to?</Form.Label>
+        </Row>
+        <br/>
+          <Row className="justify-content-md-center">
+            <Col lg="2">
+              <Form.Check 
+              onClick={(e) => {handleCheckbox("Developer")}}
+              name="Roles"
+              label="Developer" 
+              inline
+              disabled={roles.TestDev || roles.TestDes}
+              />
+            </Col>
+            <Col lg="2">
+              <Form.Check 
+              onClick={(e) => {handleCheckbox("Product Manager")}}
+              name="Roles" 
+              label="Product Manager" 
+              inline
+              disabled={roles.TestDev || roles.TestDes}
+              />
+            </Col>
+            <Col lg="2">
+              <Form.Check 
+                onClick={(e) => {handleCheckbox("TEST Designer")}} 
+                name="Roles" 
+                label="TEST Designer" 
+                inline
+                disabled={roles.Des || roles.Dev || roles.ProdMan}
+                />
+            </Col>
+          </Row>
+      
+          <Row className="justify-content-md-center">
+            <Col lg="4">
+              <Form.Check 
+              onClick={(e) => {handleCheckbox("Designer")}}
+              name="Roles" 
+              label="Designer"
+              disabled={roles.TestDev || roles.TestDes}
+              />
+            </Col>
+            <Col lg="2">
+              <Form.Check 
+              onClick={(e) => {handleCheckbox("TEST Developer")}}
+              name="Roles" 
+              label="TEST Developer"
+              disabled={roles.Des || roles.Dev || roles.ProdMan}
+              />
+            </Col>
+          
+          </Row>
+        </Container>
+      
+      </Form.Group>
 
       {/* Free Response Section below this line*/}
     </Form>
